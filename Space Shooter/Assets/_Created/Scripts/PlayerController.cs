0c8c073658dd064bd5e3c781d4337d1a15652b42
Jason Mavandi <jason.mavandi@emc.com>
2017-07-04 23:33:57 -0600

    Space shooter - boundery destroyer

--- Space Shooter/Assets/_Created/Scripts/PlayerController.cs
+++ Space Shooter/Assets/_Created/Scripts/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void Update() {
-        if ((Input.GetButton("Fire1") || Input.GetKeyDown(KeyCode.Space))&& Time.time > nextFire) {
+        if ((Input.GetButton("Fire1") || Input.GetKey(KeyCode.Space))&& Time.time > nextFire) {
             nextFire = Time.time + fireRate;
             Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
         }
